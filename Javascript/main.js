f57fac3a03db2fdfa6c446fba9c67b72b7dcd2f4
Amr-Elharery <amrelharery@gmail.com>
2023-02-01 12:25:59 +0200

    New Change

---
+++ Javascript/main.js
@@ -0,0 +1,17 @@
+let el = document.querySelector(".scroller");
+
+window.addEventListener("scroll", () => {
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
+
+
+
+// -------Functions
+function thank() {
+  alert("Thanks for sending me");
+}
+function error() {
+  alert("Error sending me..\nPlease try again");
+}
